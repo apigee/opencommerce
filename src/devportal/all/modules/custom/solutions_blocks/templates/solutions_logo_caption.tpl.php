b29f4e38bd069df79a93b145e1866d583315f6ec
seshi <seshireddy@apigee.com>
2016-07-18 17:18:56 +0530

    Devportal Customizations

---
+++ src/devportal/all/modules/custom/solutions_blocks/templates/solutions_logo_caption.tpl.php
@@ -0,0 +1,5 @@
+<div>
+  <span>
+    <a href="/<?php print drupal_get_path_alias(variable_get('site_frontpage', 'home')); ?>" title=" OpenCommerce Home"><span style="color: #84aab9;font-size:28px;">Open</span>&nbsp;<span style="color: #808080;font-size:28px;">Commerce</span></a>
+  </span>
+</div>
