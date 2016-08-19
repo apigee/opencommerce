2846ea219da38431b26265f24c88bb23f4d28e80
Prashanth K S <prashanth@retina-koo-2.local>
2016-08-19 13:57:05 +0530

    added config.sh instructions

--- src/gateway/setup/config.sh
+++ src/gateway/setup/config.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
+# URI of the Management API to the Edge Org where you wish to deploy the proxies
 URI="https://api.enterprise.apigee.com"
-UGURI="baas-ug000sr.apigee.net"
-ORG="open-commerce"
-ENV="dev"
-ADMIN_EMAIL="psubrahmanyam@apigee.com"
+# URI of the BaaS / Usergrid API which will be used as a sandbox backend
+UGURI="https://api.usergrid.com"
+# Organization name of the Edge Org where you will be deploying the proxies
+ORG=
+# Environment in the Org specified above to which you will deploy the proxies
+ENV=
+# Email of a user that has sufficient permissions to import and deploy proxies in the specified Org
+ADMIN_EMAIL=
+# Edge Password of the user specified above
 APW=
-UGORG="api-solutions"
-UGAPP="opencommerce"
-UGCLIENTID="b3U62L-r4Tx8Eeaklgpl6pGh6w"
-UGCLIENTSECRET="b3U6iTCxoJun46gKyZ_A8hfTfqOGEcE"
-
-
-
-#URI="https://api.enterprise.apigee.com"
-#UGURI="api.usergrid.com"
-#ORG="open-commerce"
-#ENV="dev"
-#ADMIN_EMAIL="psubrahmanyam@apigee.com"
-#APW=
-#UGORG="rmahalank"
-#UGAPP="commerce"
-#UGCLIENTID="b3U6pNCYGrQtEeW0_5XptG87wA"
-#UGCLIENTSECRET="b3U62QY-UCjvBH3mRt-7yPaS_WKYBcQ"
+# Organization name of the BaaS instance that is being used to deploy the sandbox
+UGORG=
+# Application in the above BaaS instance which will be used for the sandbox. It will be created if it doesn't exist.
+UGAPP=
+# Organization Client ID of the BaaS where you wish to install the sandbox
+UGCLIENTID=
+# Organization Client Secret of the BaaS where you wish to install the sandbox
+UGCLIENTSECRET=
