42dfcc2afe8cac71eba8c648dbbd2534822cd255
Muthukkannan Alagappan <malagappan@apigee.com>
2016-08-19 13:10:48 +0530

    Corrected

--- src/gateway/setup/config.sh
+++ src/gateway/setup/config.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
+#Management URI
 URI="https://api.enterprise.apigee.com"
-UGURI="baas-ug000sr.apigee.net"
-ORG="open-commerce"
-ENV="dev"
-ADMIN_EMAIL="psubrahmanyam@apigee.com"
+
+#Baas URI
+UGURI=
+
+#OrgName
+ORG=
+
+#Environment (test/prod/...)
+ENV=
+
+#AdminEmail
+ADMIN_EMAIL=
+
+#Password
 APW=
-UGORG="api-solutions"
+
+#Baas Org Name
+UGORG=
+
+#Baas App Name
 UGAPP="opencommerce"
-UGCLIENTID="b3U62L-r4Tx8Eeaklgpl6pGh6w"
-UGCLIENTSECRET="b3U6iTCxoJun46gKyZ_A8hfTfqOGEcE"
 
+#BaaS App Client-Id
+UGCLIENTID=
 
+#Baas Client Secret
+UGCLIENTSECRET=
 
-#URI="https://api.enterprise.apigee.com"
-#UGURI="api.usergrid.com"
-#ORG="open-commerce"
-#ENV="dev"
-#ADMIN_EMAIL="psubrahmanyam@apigee.com"
-#APW=
-#UGORG="rmahalank"
-#UGAPP="commerce"
-#UGCLIENTID="b3U6pNCYGrQtEeW0_5XptG87wA"
-#UGCLIENTSECRET="b3U62QY-UCjvBH3mRt-7yPaS_WKYBcQ"
