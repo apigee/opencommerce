535d2b1d101d34dc13f30f2ba2e65f3683e837fb
Muthukkannan Alagappan <malagappan@apigee.com>
2016-08-24 14:40:18 +0530

    Added VerifyScope for Carts

---
+++ src/gateway/carts/apiproxy/resources/jsc/VerifyScope.js
@@ -0,0 +1,33 @@
+var grant_type = context.getVariable('grant_type');
+var scope = context.getVariable('scope');
+
+print('grant_type: ' + grant_type);
+print('scope: ' + scope);
+
+var isError = true;
+
+if (scope) {
+    var scopes = scope.split(' ');
+    for (var i=0; i < scopes.length; i++) {
+        if (scopes[i].toLowerCase() === 'carts') {
+            isError = false;
+            break;
+        }
+    }
+}
+
+if (grant_type && grant_type == 'client_credentials') {
+    var user_name = context.getVariable('request.queryparam.user_id');
+    if (user_name) {
+        context.setVariable('username', user_name);
+    }
+} else {
+    var user_name = context.getVariable('accesstoken.username');
+    if (user_name) {
+        context.setVariable('username', user_name)
+    }
+}
+
+if (isError) {
+    context.setVariable('scopeError','Invalid Scope');
+}
